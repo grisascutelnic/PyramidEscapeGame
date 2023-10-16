104859fbccf4ca3351f0fc1020113775919faffc
Dumitru Frimu <dumitrufrimu118@gmail.com>
2023-10-17 02:00:00 +0300

    Experiment2

--- src/main/java/com/game/pyramidescapegame/ExperimentDima.java
+++ src/main/java/com/game/pyramidescapegame/ExperimentDima.java
@@ -1,5 +1,18 @@
 package com.game.pyramidescapegame;
 
-public class ExperimentDima {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class ExperimentDima extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
 }
