3de053e8739e345b18c6b1956a4392ac5c14890e
Grigore Scutelnic <scutelnic.grigore10@gmail.com>
2023-10-17 08:22:36 +0300

    Am facut navigarea de pe o pagina pe alta

--- src/main/java/experimente/ExperimentGrisa.java
+++ src/main/java/experimente/ExperimentGrisa.java
@@ -1,5 +1,19 @@
 package experimente;
 
-public class ExperimentGrisa {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class ExperimentGrisa extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
 }
+
